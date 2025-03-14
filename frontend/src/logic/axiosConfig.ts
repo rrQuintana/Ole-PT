c51f0e470148f81f0f3f9bb5f56ddc3fb9a101cb
Roberto Quintana <rrquintana7@gmail.com>
2025-03-13 19:19:58 -0600

    Update axiosConfig.ts

--- frontend/src/logic/axiosConfig.ts
+++ frontend/src/logic/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://ole-pt.onrender.com/api/v1/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
